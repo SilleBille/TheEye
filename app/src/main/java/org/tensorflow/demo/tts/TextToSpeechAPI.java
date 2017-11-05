81759710e24925da8ab028f0bd5afe1fbe2f10f7
Dinesh Prasanth M K <mkdineshprasanth@gmail.com>
2017-11-05 05:47:39 -0500

    Tensorflow works with voice recognition

---
+++ app/src/main/java/org/tensorflow/demo/tts/TextToSpeechAPI.java
@@ -0,0 +1,20 @@
+package org.tensorflow.demo.tts;
+
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+
+/**
+ * Created by gsudan92 on 11/4/2017.
+ */
+
+public class TextToSpeechAPI {
+
+    public static void speak(TextToSpeech tts, String text){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }else{
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+}
